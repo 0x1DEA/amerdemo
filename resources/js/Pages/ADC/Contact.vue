4c93bb5f8486bc4ba1ca9d607bb59cd6cfa5c40f
0x1DEA <seebeyondedm@gmail.com>
2023-03-30 16:53:35 -0500

    new images!

--- resources/js/Pages/ADC/Contact.vue
+++ resources/js/Pages/ADC/Contact.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import CoolBox from "./../../Components/CoolBox.vue";
-import hero1 from "./../../../assets/home-rotator-1.jpg";
-import hero2 from "./../../../assets/home-rotator-3.jpg";
+import hero2 from "./../../../assets/conference.jpg";
 import Layout from "../../Layouts/Layout.vue";
 import { Head } from '@inertiajs/vue3'
 
@@ -15,24 +14,31 @@ import { Head } from '@inertiajs/vue3'
             <div class="y relative md:py-8 px-8 space-y-8 w-full lg:max-w-5xl xl:max-w-6xl">
                 <div class="absolute p-20 blockgrid right-12 top-0"></div>
                 <div class="absolute p-32 blockgrid left-12 top-[18rem]"></div>
-                <div class="relative flex flex-col md:flex-row items-center w-full justify-between gap-8 !mb-4">
-                    <div class="y gap-8 md:w-2/3">
-                        <div class=" y text-white font-bold font-mono text-6xl">
+                <div class="relative y items-center w-full justify-between gap-8 !mb-4">
+                    <div class="flex items-center flex-col lg:flex-row w-full lg:justify-between gap-8">
+                        <div class="y text-white font-bold font-mono text-6xl">
                             <span>CONTACT</span>
                             <span class="bg-white text-black w-fit">US</span>
                         </div>
-                        <CoolBox class="max-w-full" title="Thank You">
-                            Thank you for your interest in American Demolition Corporation. Headquartered in Carol Stream, Illinois – just 30 minutes from downtown Chicago – American Demolition provides complete demolition services to clients throughout the Midwest and nationwide. If you have any questions, need more information or would like to discuss an upcoming project, please contact us, and a representative will promptly respond to your request or inquiry.
-                        </CoolBox>
                     </div>
-                    <div class="y gap-4 text-white md:w-1/3">
-                        <h2 class="font-bold text-3xl">American Demolition Corporation</h2>
-                        <ul class="space-y-2 list-inside list-[square] text-lg">
-                            <li>480 Randy Rd. Carol Stream, IL 60188</li>
-                            <li>Phone: 847-608-0010</li>
-                            <li>Fax: 847-608-0060</li>
-                            <li>Email: info@amerdemo.com</li>
-                        </ul>
+                    <div class="flex flex-col items-center lg:flex-row gap-8 w-full">
+                        <div class="mr-3 mb-1" style="filter: drop-shadow(0.75rem 0.25rem #FBBF24);">
+                            <img class="w-[32rem] float-right" :src="hero2" alt="Hero" style="clip-path: polygon(0 0,calc(100% - 3rem) 0,100% 3rem,100% 100%,3rem 100%,0 calc(100% - 3rem));"/>
+                        </div>
+                        <div class="y space-y-4">
+                            <CoolBox class="max-w-full" title="Thank You">
+                                Thank you for your interest in American Demolition Corporation. Headquartered in Carol Stream, Illinois – just 30 minutes from downtown Chicago – American Demolition provides complete demolition services to clients throughout the Midwest and nationwide. If you have any questions, need more information or would like to discuss an upcoming project, please contact us, and a representative will promptly respond to your request or inquiry.
+                            </CoolBox>
+                            <div class="y text-white pl-8">
+                                <h2 class="font-bold text-3xl">American Demolition Corporation</h2>
+                                <ul class="space-y-2 list-inside list-[square] text-lg">
+                                    <li>480 Randy Rd. Carol Stream, IL 60188</li>
+                                    <li>Phone: 847-608-0010</li>
+                                    <li>Fax: 847-608-0060</li>
+                                    <li>Email: info@amerdemo.com</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="relative flex flex-col md:flex-row items-center w-full justify-between gap-8">
--- resources/js/Pages/ADC/Home.vue
+++ resources/js/Pages/ADC/Home.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import CoolBox from "./../../Components/CoolBox.vue";
-import hero1 from "./../../../assets/home-rotator-1.jpg";
-import hero2 from "./../../../assets/home-rotator-3.jpg";
+import hero1 from "./../../../assets/warehouse.jpg";
+import hero2 from "./../../../assets/group.jpg";
 import Layout from "../../Layouts/Layout.vue";
 import { Head } from '@inertiajs/vue3'
 
@@ -13,16 +13,18 @@ import { Head } from '@inertiajs/vue3'
         </Head>
         <main class="y items-center grow">
             <div class="y relative md:py-16 px-8 space-y-8 w-full lg:max-w-5xl xl:max-w-6xl">
-                <div class="y text-white font-bold font-mono text-6xl mt-4">
-                    <span>AMERICAN</span>
-                    <span class="bg-white text-black w-fit">DEMOLITION</span>
-                </div>
                 <div class="absolute p-20 blockgrid right-12 top-0"></div>
                 <div class="absolute p-32 blockgrid left-12 top-[18rem]"></div>
                 <div class="relative flex flex-col md:flex-row w-full justify-between gap-8">
-                    <CoolBox title="Start To Finish">
-                        American Demolition Corporation is focused on your complete satisfaction. Whether your project is large or small, we strive to provide the highest level of service possible in completing projects on time, within budget and without surprises or complications.
-                    </CoolBox>
+                    <div class="y gap-8">
+                        <div class="y text-white font-bold font-mono text-6xl mt-4">
+                            <span>AMERICAN</span>
+                            <span class="bg-white text-black w-fit">DEMOLITION</span>
+                        </div>
+                        <CoolBox title="Start To Finish">
+                            American Demolition Corporation is focused on your complete satisfaction. Whether your project is large or small, we strive to provide the highest level of service possible in completing projects on time, within budget and without surprises or complications.
+                        </CoolBox>
+                    </div>
                     <div class="mr-3 mb-1" style="filter: drop-shadow(0.75rem 0.25rem #FBBF24);">
                         <img class="w-[32rem] float-right" :src="hero1" alt="Hero" style="clip-path: polygon(0 0,calc(100% - 3rem) 0,100% 3rem,100% 100%,3rem 100%,0 calc(100% - 3rem));"/>
                     </div>
